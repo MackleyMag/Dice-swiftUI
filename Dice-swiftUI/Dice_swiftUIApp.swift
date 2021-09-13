750ea7a8877e441dfa715822aca8f34cb6bbb0fa
MackleyMag <mackleymagalhaess@hotmail.com>
2021-09-13 14:53:34 -0300

    Initial Commit

---
+++ Dice-swiftUI/Dice_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_swiftUIApp.swift
+//  Dice-swiftUI
+//
+//  Created by Mackley Magalhães da Silva on 13/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Dice_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
